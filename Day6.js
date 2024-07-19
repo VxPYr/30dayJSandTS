e68fa0f1f17efed831fbeecf3302c827072630b7
VxPYr <vishnujaswal2004@gmail.com>
2024-07-19 09:43:34 +0530

    Day-6 complete

--- Day6.js
+++ Day6.js
@@ -1,6 +1,6 @@
 // Activity-1: Array Creation and Access
 // Task-1
-let arr = [1,2,3,4,5];
+let arr = [1,2,30,4,50];
 let arr1 = [5,4,10,23,31];
 
 console.log(arr);
@@ -33,3 +33,29 @@ console.log(map1);
 // Task-8
 let filter1 = arr1.filter((i)=> i<10);
 console.log(filter1);
+
+// Task-9
+let reduce1 = arr.reduce((i, currentValue) => {
+    console.log(`i = ${i} and currentValue = ${currentValue} => sum = ${i+currentValue}`)
+    return i + currentValue;
+},0);
+console.log(reduce1);
+
+// Activity-4: Array Iteration
+// Task-10
+for(let i = 0; i<arr.length; i++){
+    console.log(arr[i]);
+}
+
+// Task-11
+arr.forEach(element => {
+    console.log(element);
+});
+
+// Activity-5: Multi-Dimensional Array
+// Task-12
+let multiArr= [[1,2,3,4],[5,6,7,8]];
+console.log(multiArr)
+
+// Task-13
+console.log(multiArr[0][3]);
